56390464ea3bb9b2a320b48b7cc5d86c5deff1f3
cristianilisei96 <cristianilisei96@gmail.com>
2021-09-21 23:39:11 +0300

    security test

--- src/js/admin.js
+++ src/js/admin.js
@@ -6,6 +6,56 @@ const productsAdminURL = 'https://61363d1a8700c50017ef54c3.mockapi.io/products/'
 // Get elem from html
 const loginPage = document.getElementById('loginPage');
 const appPage = document.getElementById('appPage');
+
+const inputs = [
+    document.getElementById('emailFieldAdmin'),
+    document.getElementById('passwordFieldAdmin')
+]
+
+inputs.forEach((input) => {
+    input.addEventListener('keydown', handler);
+    input.addEventListener('input', handler);
+});
+
+function handler(e) {
+    if (e.type === 'input') {
+        validate(e.target);
+    } else {
+        submitOnEnterKey(e);
+    }
+}
+
+function validate(target) {
+    const result = checkLength(target);
+
+    if (result) {
+        target.classList.remove('is-invalid');
+    } else {
+        target.classList.add('is-invalid');
+    }
+
+    return result;
+}
+
+function checkLength(target) {
+    return !!target.value;
+}
+
+function submitOnEnterKey(e) {
+    if (e.keyCode === 13) {
+        submitForm(e);
+        if (e.cancelable) {
+            e.preventDefault();
+        }
+    }
+}
+
+function submitForm(e) {
+    const result = inputs.map((input) => validate(input)).every((val) => val);
+
+    e.preventDefault();
+}
+
 const emailFieldAdmin = document.getElementById('emailFieldAdmin');
 const passwordFieldAdmin = document.getElementById('passwordFieldAdmin');
 const loginBtn = document.getElementById('loginBtn');
@@ -13,25 +63,24 @@ const loginBtn = document.getElementById('loginBtn');
 var objAdmin = [
     {
         email: "cristianilisei96@gmail.com",
-        password: "Parolamea1"
+        password: "Admin1"
     }, 
     {
         email: "admin@gmail.com",
-        password: "admin"
+        password: "Admin2"
     }
 ];
 
 loginBtn.addEventListener('click', (e) => {
     var email = emailFieldAdmin.value;
     var password = passwordFieldAdmin.value;
+    const result = inputs.map((input) => validate(input)).every((val) => val);
+    
     e.preventDefault();
     for(let i = 0; i < objAdmin.length; i++) {
         if( email == objAdmin[i].email && password == objAdmin[i].password ){
-            console.log(email + " is logged in!");
             loginPage.classList.add('d-none');
             appPage.classList.add('d-block');
-        } else {
-            console.log('Try again !');
         }
     }
 });
