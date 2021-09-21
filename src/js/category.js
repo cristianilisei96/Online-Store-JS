11872658a1e1cb4a9960d456e4149490c0656a1d
cristianilisei96 <cristianilisei96@gmail.com>
2021-09-21 16:40:17 +0300

    category js done

--- src/js/category.js
+++ src/js/category.js
@@ -1,32 +1,17 @@
 import { http } from './http.js';
 import { ui } from './ui.js';
 
-// Get category products URL
-const macProductsUrl = 'https://61363d1a8700c50017ef54c3.mockapi.io/products?category=mac';
-const iphoneProductsUrl = 'https://61363d1a8700c50017ef54c3.mockapi.io/products?category=iphone';
-const ipadProductsUrl = 'https://61363d1a8700c50017ef54c3.mockapi.io/products?category=ipad';
-const watchProductsUrl = 'https://61363d1a8700c50017ef54c3.mockapi.io/products?category=watch';
-const tvProductsUrl = 'https://61363d1a8700c50017ef54c3.mockapi.io/products?category=tv';
-const musicProductsUrl = 'https://61363d1a8700c50017ef54c3.mockapi.io/products?category=music';
-const accessoriesProductsUrl = 'https://61363d1a8700c50017ef54c3.mockapi.io/products?category=accessories';
-
-// Get details of product selected
+// Get category product URL
 let categoryParamSearch = window.location.search;
 const searchParam = new URLSearchParams(categoryParamSearch).get('filter');
 const detailsProductUrl = 'https://61363d1a8700c50017ef54c3.mockapi.io/products?category=' + searchParam;
-console.log('DetailsProductUrl is ' + detailsProductUrl);
-console.log('SearchParam is ' + searchParam);
-console.log('CategoryParamSearch is ' + categoryParamSearch);
-// http.get(detailsProductUrl).then((product) => ui.showProductDetails(product));
 
 // Get location on app
 let currentLocation = window.location;
-console.log('CurrentLocation is ' + currentLocation);
 
 const categoryFilterURL = 'https://61363d1a8700c50017ef54c3.mockapi.io/products?category=' + searchParam;
 
-if(window.location.href.indexOf(searchParam) != -1) {
-    console.log('true');
+if(currentLocation.href.indexOf(searchParam) != -1) {
 
     http.get(categoryFilterURL).then((products) => ui.showProductsToUsers(products));
 
@@ -46,44 +31,4 @@ if(window.location.href.indexOf(searchParam) != -1) {
         document.getElementById('link-sidebar-accessories-filter').classList.add('bg-success');
     }
     
-} else {
-    console.log('false');
-}
-
-// if(currentLocation == 'http://localhost:5000/category?filter=mac'){
-    
-//     http.get(macProductsUrl).then((products) => ui.showProductsToUsers(products));
-
-//     document.getElementById('link-sidebar-mac-filter').classList.add('bg-success');
-
-// } else if(currentLocation == 'http://localhost:5000/category?filter=iphone'){
-    
-//     http.get(iphoneProductsUrl).then((products) => ui.showProductsToUsers(products));
-//     document.getElementById('link-sidebar-iphone-filter').classList.add('bg-success');
-
-// } else if(currentLocation == 'http://localhost:5000/category?filter=ipad'){
-    
-//     http.get(ipadProductsUrl).then((products) => ui.showProductsToUsers(products));
-//     document.getElementById('link-sidebar-ipad-filter').classList.add('bg-success');
-
-// } else if(currentLocation == 'http://localhost:5000/category?filter=watch'){
-    
-//     http.get(watchProductsUrl).then((products) => ui.showProductsToUsers(products));
-//     document.getElementById('link-sidebar-watch-filter').classList.add('bg-success');
-
-// } else if(currentLocation == 'http://localhost:5000/category?filter=tv'){
-    
-//     http.get(tvProductsUrl).then((products) => ui.showProductsToUsers(products));
-//     document.getElementById('link-sidebar-tv-filter').classList.add('bg-success');
-
-// } else if(currentLocation == 'http://localhost:5000/category?filter=music'){
-    
-//     http.get(musicProductsUrl).then((products) => ui.showProductsToUsers(products));
-//     document.getElementById('link-sidebar-music-filter').classList.add('bg-success');
-
-// } else if(currentLocation == 'http://localhost:5000/category?filter=accessories'){
-    
-//     http.get(accessoriesProductsUrl).then((products) => ui.showProductsToUsers(products));
-//     document.getElementById('link-sidebar-accessories-filter').classList.add('bg-success');
-
-// } else {}
+} else {}
