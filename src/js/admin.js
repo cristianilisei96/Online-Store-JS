4adedfed2403cbbb902c89ea28fdacbbd93ac9d8
cristianilisei96 <cristianilisei96@gmail.com>
2021-09-21 23:10:55 +0300

    security test

--- src/js/admin.js
+++ src/js/admin.js
@@ -3,8 +3,44 @@ import { ui } from './ui.js';
 
 const productsAdminURL = 'https://61363d1a8700c50017ef54c3.mockapi.io/products/';
 
-document.getElementById('add-product').addEventListener('click', addNewProduct);
-document.getElementById('products-container').addEventListener('click', deleteProduct);
+// Get elem from html
+const loginPage = document.getElementById('loginPage');
+const appPage = document.getElementById('appPage');
+const emailFieldAdmin = document.getElementById('emailFieldAdmin');
+const passwordFieldAdmin = document.getElementById('passwordFieldAdmin');
+const loginBtn = document.getElementById('loginBtn');
+
+var objAdmin = [
+    {
+        email: "cristianilisei96@gmail.com",
+        password: "Parolamea1"
+    }, 
+    {
+        email: "admin@gmail.com",
+        password: "admin"
+    }
+];
+
+loginBtn.addEventListener('click', (e) => {
+    var email = emailFieldAdmin.value;
+    var password = passwordFieldAdmin.value;
+    e.preventDefault();
+    for(let i = 0; i < objAdmin.length; i++) {
+        if( email == objAdmin[i].email && password == objAdmin[i].password ){
+            console.log(email + " is logged in!");
+            loginPage.classList.add('d-none');
+            appPage.classList.add('d-block');
+        } else {
+            console.log('Try again !');
+        }
+    }
+});
+
+const homepageBtn = document.getElementById('homepageBtn');
+
+homepageBtn.addEventListener('click', () => {
+    window.location.href = "index.html";
+});
 
 // http.get(productsAdminURL).then((products) => ui.showProductsToAdmins(products));
 
@@ -13,6 +49,9 @@ document.addEventListener('DOMContentLoaded', getProducts);
 function getProducts() {
     http.get(productsAdminURL).then((products) => ui.showProductsToAdmins(products));
 }
+
+document.getElementById('add-product').addEventListener('click', addNewProduct);
+document.getElementById('products-container').addEventListener('click', deleteProduct);
 
 function addNewProduct() {
     var nameValue = document.getElementById('title').value;
